e7936c2505ba77790f51dcd15a29bcb8d17a6c97
masprog2022 <masprog2022@gmail.com>
2022-01-19 14:28:26 +0100

    melhorando teste de integracao com @DataJpaTest

--- backend/src/test/java/com/maurodev/minhasfinancas/repositories/UsuarioRepositoryTest.java
+++ backend/src/test/java/com/maurodev/minhasfinancas/repositories/UsuarioRepositoryTest.java
@@ -1,21 +1,22 @@
 package com.maurodev.minhasfinancas.repositories;
 
 import org.assertj.core.api.Assertions;
-
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.extension.ExtendWith;
-
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase.Replace;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ActiveProfiles;
-import org.springframework.test.context.junit.jupiter.SpringExtension;
 
 import com.maurodev.minhasfinancas.entities.Usuario;
 
 
-@ExtendWith(SpringExtension.class)
+
 @SpringBootTest
 @ActiveProfiles("test")
+@DataJpaTest
+@AutoConfigureTestDatabase(replace = Replace.NONE)
 public class UsuarioRepositoryTest {
 	
 	@Autowired
